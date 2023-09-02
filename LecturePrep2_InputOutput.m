1ad168feaf3ccbcfd8cc7665a6bcabb4305275fa
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-09-01 23:13:00 -0400

    Back-up save 2 (MATLAB might crash)

--- LecturePrep2_InputOutput.m
+++ LecturePrep2_InputOutput.m
@@ -2,13 +2,16 @@
 % ENGR 130
 % Lecture Preparation 2
 % 9/1/23
+
+%% INPUT AND OUTPUT IN MATLAB
+%% Input
 %% Input Method 1: Command Window
-    % Memory is shared by command window and script file so data can be
+    % - Memory is shared by command window and script file so data can be
     % interchanged
 %% Input Method 2: Input Command
     % input()
-        % Prompts user for input
-        % Can only take one value and assign to variable
+        % - Prompts user for input
+        % - Can only take one value and assign to variable
             % Either numeric or text (requires more effort)
                 % Numeric Input
                     % Three pieces of info needed
@@ -16,17 +19,31 @@
                         % input() command
                         % Prompt to user
                     % Example
-                        ageEx = input('Enter the age of the birthday child.\n')
+                        ageEx = input('Enter the age of the birthday child.\n');
                             % \n means new line like Java, C++, etc.
                 % Character Input
                     % Include 's' after prompt to indicate input is text
                     % Example
-                        choice = input('Donut?\n','s')
+                        choice = input('Donut?\n','s');
                             % 's' short for "string of characters"
                             % Make sure the prompt is clear... what is this
                             % input() prompt asking for?
 %% Practice 1
-% Write a script that asks the user for their first name and age. Assign
-% each value to a variable.
-firstName = input("What's your name?\n",'s')
-age = input('How old are you?\n')
+    % Write a script that asks the user for their first name and age. Assign
+    % each value to a variable.
+    firstName = input("What's your name?\n",'s');
+    age = input('How old are you?\n');
+
+%% Output
+%% Output Method 1: No Semicolon
+    gold = 72
+    % Result will print in Command Window
+%% Output Method 2: disp()
+    % Displays value of variable (numbers of text)
+    clc;
+
+    gold2 = 72;
+    element = 'Gold';
+    disp(gold2) % stored numeric variable displayed
+    disp(element) % stored text variable displayed
+    disp('material') % exact text displayed
