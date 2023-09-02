f3dd49951bc540375a0c69270e4d3cf5fa7c17bd
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-09-01 22:26:25 -0400

    Adding old example files

---
+++ ENGR130HomeworkExample.m
@@ -0,0 +1,19 @@
+% Kathy Harper
+% ENGR 130 Homework 1
+% January 1st, 2023
+%% Problem 1
+clear;
+clc;
+close all;
+
+disp("Hello world!") 
+%% Problem 2
+clear;
+clc;
+close all;
+
+% Print squares of the numbers from 1 to 5
+for num = 1:1:5
+  num_squared = num^2;
+    	  fprintf('The square of %i is %i.\n', num, num_squared);
+end
---
+++ ENGR130LabExample.m
@@ -0,0 +1,24 @@
+% Kathy Harper, Kurt Rhoads, James Schmidt
+% ENGR 130 Module 0 Report
+% Section ####
+% January 1st, 2023
+%% Lab 1 (%% means section break)
+clear;
+clc;
+close all;
+
+% Part 1: Assign values to radii of each circle in cm
+radius_1 = 5.0;
+radius_2 = 7.5;
+
+% Part 2: Calculate the area of each circle
+area_1 = pi * radius_1^2;
+area_2 = pi * radius_2^2;
+%% Lab 2
+clear;
+clc;
+close all;
+
+% Part 1: Display the area of each circle, properly labeled
+fprintf('The area of a circle radius of %f is %f.\n', radius_1, area_1);
+fprintf('The area of a circle radius of %f is %f.\n\n', radius_2, area_2);
