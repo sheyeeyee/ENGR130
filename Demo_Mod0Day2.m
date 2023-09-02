ce33d5fde6bc8924d07a9d06d25cc80b987f4f83
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-09-01 22:20:29 -0400

    Module 0 Day 2
    Introduction to MATLAB. demonstration code with comment headers, common commands (clear, clc, close all), and calculations

---
+++ Demo_Mod0Day2.m
@@ -0,0 +1,13 @@
+% Shelley Wei
+% ENGR 130
+% Module 0 Lab 2
+% 8/31/23
+
+clear; % clear workspace
+clc; % clear command window
+close all; % close all windows?
+
+mass = 30;
+acc = .34
+
+Force = mass * acc
