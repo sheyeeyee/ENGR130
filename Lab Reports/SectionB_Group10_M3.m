d5c8c63c51dc4fcc336f59be842cff716d8c4fe2
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-10-10 10:14:37 -0400

    Module 3 Lab 3

--- Lab Reports/SectionB_Group10_M3.m
+++ Lab Reports/SectionB_Group10_M3.m
@@ -11,15 +11,15 @@ clear;
 clc;
 close all;
 
-% 3b
+% 3b.
 % set up Arduino
-A = arduino();
+a = arduino();
 
 % turn the light on & off at a frequency of 1 Hz over a period of 30 seconds
 for i = 1:30
-    writeDigitalPin(A, "D8", 1);
+    writeDigitalPin(a, "D8", 1);
     pause(0.5);
-    writeDigitalPin(A, "D8", 0);
+    writeDigitalPin(a, "D8", 0);
     pause(0.5)
 end
 
@@ -29,7 +29,7 @@ clear;
 clc;
 close all;
 
-% 2a
+% 2a.
 piezoKnock = readmatrix('SectionB_Group10_M3_1.csv'); % importing data from oscilloscope
 
 piezoKnock = piezoKnock(4:1667, :); % removing NaN values from original data
@@ -39,7 +39,7 @@ piezoKnock = piezoKnock(4:1667, :); % removing NaN values from original data
 [piezoKnockMax, maxIdx] = max(piezoKnock(:, 2)); % finding the maximum voltage and the index of it to plot later
 piezoKnockMean = mean(piezoKnock(:, 2)); % finding the mean voltage to plot later
 
-% 2b
+% 2b.
 hold on;
 plot(piezoKnock(:, 1), piezoKnock(:, 2), "k"); % actual data
 plot(piezoKnock(maxIdx), piezoKnockMax, "bo"); % max point
@@ -54,4 +54,55 @@ ylabel("Voltage");
 title("Piezoelectric Voltage Data from Knocks");
 
 %% LAB 3: DETECTING KNOCKS
-%%
+% 3. Write a knock sensor script in MATLAB
+clear;
+clc;
+close all;
+
+% 3a.
+a = arduino();
+voltages = [];
+timeList = 1:40; % 4 Hz means 4 readings per second, so over 10 seconds there will be 40 readings
+
+for index = timeList
+    voltages(index) = readVoltage(a, 'A1');
+    disp(voltages(index)); % sanity check
+    pause(0.25); % pause for 0.25 seconds between each reading which means 4 readings per second
+end
+
+disp(voltages); % sanity check
+
+% 3b.
+hold on;
+plot(timeList, voltages);
+xlabel("Time in quarter-seconds");
+ylabel("Voltages in volts");
+title("Voltages of Knocks Over Time");
+  
+% 3c.
+thresh = 0.06;
+plot(timeList(voltages >= thresh), voltages(voltages >= thresh), "ro");
+yline(thresh, "--");
+
+%% 3e.
+a = arduino();
+voltages = [];
+timeList = 1:40;
+
+thresh = 0.06;
+
+countKnock = 0;
+firstKnock = 1;
+
+for index = timeList
+    voltages(index) = readVoltage(a, 'A1');
+    if (voltages(index) >= thresh) & (firstKnock == 1) % checking for the first time the voltage goes beyond threshold since each knock is an up-down peak
+        countKnock = countKnock + 1;
+        firstKnock = 0; % set this to 0 to indicate that the voltage passed the threshold for the first time during a knock
+    elseif (voltages(index) <= thresh) & (firstKnock == 0) % checking for when voltage goes back down below the threshold so we know that the knock has ended and the next one can be counted
+        firstKnock = 1;
+    end
+    disp(voltages(index)); % sanity check
+    pause(0.25);
+end
+fprintf("The number of knocks recorded is %i.\n", countKnock);
