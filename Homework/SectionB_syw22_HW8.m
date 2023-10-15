5b39b590f5fb9175679b73d56ab7347d4f718e8e
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-10-14 21:26:25 -0400

    Submitted HW 8 and finished Lecture Prep 8

--- Homework/SectionB_syw22_HW8.m
+++ Homework/SectionB_syw22_HW8.m
@@ -2,7 +2,7 @@
 % ENGR 130
 % Homework 8
 % Started 10/14/23
-% Submitted 10//23
+% Submitted 10/14/23
 % Due 10/18/23
 
 %% Question 2
@@ -13,9 +13,11 @@ close all;
 load('HW8_gait_data.mat');
 
 % Part 1
+% sort cadences and strides in ascending order so the graph is readable
 cadenceSort = sort(cadence);
 strideSort = sort(stride_length);
 
+% create a plot with cadences and strides
 figure(1);
 plot(strideSort, cadenceSort);
 title("Relationship Between Stride Length and Stride Cadence");
@@ -26,9 +28,10 @@ strideRange = max(stride_length) - min(stride_length); % the range() function on
 fprintf("The range of stride lengths is approximately %.2f cm.\n", strideRange);
 
 % Part 2
-strideSegment = strideRange/8;
-strideStdev = zeros(1,8);
+strideSegment = strideRange/8; % divide the range by 8 to get eight segments/groups
+strideStdev = zeros(1,8); % create a vector of eight 0s to compile the stride lengths in the different deviations
 
+% count how many patients have a stride length in each segment
 for i = 1:length(strideSort)
     if (strideSort(i) <= strideSegment)
         strideStdev(1) = strideStdev(1) + 1;
@@ -49,6 +52,7 @@ for i = 1:length(strideSort)
     end
 end
 
+% create a second plot with std dev
 figure(2);
 bar(strideStdev);
 title("Standard Deviation of Stride Lengths");
@@ -56,13 +60,13 @@ xlabel("Standard Deviation");
 ylabel("Number of Stride Lengths per Standard Deviation");
 
 % Part 3
-strideTime = cadence./120; % calculate the time length of the avg stride ?????????????????
-stanceSwing = stance + swing;
-diff = abs(strideTime - stanceSwing);
+strideTime = 60 ./ cadence; % calculate the time length of the avg stride by converting min to sec and reciprocating to get time/step instead of step/time
+stanceSwing = stance + swing; % add stance and swing time
+diff = abs(strideTime - stanceSwing); % create a vector for differences in time btwn avg stride time and stance + swing time
 
-discrepant = diff(diff > 0.01); % find how many patients have discrepant data
+discrepant = diff(diff > 0.01); % find how many patients have discrepant data (their avg stride time and stance + swing time aren't the same)
 disIdx = find(diff > 0.01); % find the indices of the patients with discrepant data (finding where the condition doesn't return 0)
-disPatient = patient(disIdx);
+disPatient = patient(disIdx); % find the patients at those indices
 
 % forgot how to find elements at certain indices so just used for-loop, figured out how to use logical indexing later
 % disPatient = [];
---
+++ Lecture Preparation/LecturePrep8_Functions.m
@@ -0,0 +1,45 @@
+% Shelley Wei
+% ENGR 130
+% Lecture Preparation 8
+% Started 10/14/23
+% Finished 10/14/23
+% Due 10/17/23
+
+%% Calling a Function
+clear;
+clc;
+close all;
+
+sin(0.5)
+sin_of_angle_a = sin(0.5);
+
+%% Function Format
+% function [output variables] = functionName(input variables)
+%     block comments
+%     operations using input variables
+%           - can call other functions
+%           - output variables defined in function body
+% end
+
+
+%% Example 1A
+clear;
+clc;
+close all;
+
+% squareIt(15);
+% n = 4;
+n = input("Please enter a number: \n");
+nSquared = squareIt(n);
+fprintf("The value of %i squared is %i.\n", n, nSquared);
+
+% function definitions must be at the end of a script so function calls must be before the function definition
+function squareVal = squareIt(num) % squareVal's and num's scope is in the function
+% This function calculates the square of a number
+% Format of call: square(num)
+% Input
+%     num: number to be squared
+% Output
+%     squareVal: square of the number
+squareVal = num * num;
+end
