db766fed330d19f30ae00c45e8eeb258313ec5a5
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-10-17 16:15:33 -0400

    Setting up for different parts of Lab 1

--- Lab Reports/SectionB_Group10_M4.m
+++ Lab Reports/SectionB_Group10_M4.m
@@ -16,12 +16,12 @@ y = [1]; % m
 vel = 0; % m/s
 dt = 0.0001; % s (time increment)
 t = 10; % s (total time for ball to fall/bounce)
-c = 0; % N*s/m
 k = 0; % N/m
+c = 0; % N*s/m
 g = 9.81; % m/s^2
 
 for i = 1:t/dt
-    if (y(t) > 0)
+    if (y(t) > 0) % it keeps throwing an error saying that the index can't exceed 1, which I kind of get why but I also don't
         a = accel(m, g, k, y(t), c, vel);
         y = [y, y + vel*dt];
         vel = vel + a*dt;
@@ -41,6 +41,47 @@ ylabel("Ball Height (m)");
 % 1d.
 min_height = min(y);
 
+% 1g.
+comet(t, y, 0.01);
+
+%% 2. Try different c and k values
+%% 1st set of k and c values
+m = 1; % kg
+y = [1]; % m
+vel = 0; % m/s
+dt = 0.0001; % s (time increment)
+t = 10; % s (total time for ball to fall/bounce)
+k = 8000; % N/m
+c = 2; % N*s/m
+g = 9.81; % m/s^2
+
+
+
+%% 2nd set of k and c values
+m = 1; % kg
+y = [1]; % m
+vel = 0; % m/s
+dt = 0.0001; % s (time increment)
+t = 10; % s (total time for ball to fall/bounce)
+k = 5000; % N/m
+c = 4; % N*s/m
+g = 9.81; % m/s^2
+
+
+
+%% 3rd set of k and c values
+m = 1; % kg
+y = [1]; % m
+vel = 0; % m/s
+dt = 0.0001; % s (time increment)
+t = 10; % s (total time for ball to fall/bounce)
+k = 5000; % N/m
+c = 10; % N*s/m
+g = 9.81; % m/s^2
+
+
+
+%% Function Definitions
 function acceleration = accel(m, g, k, y, c, vel)
     % Calculates acceleration of ball
     % Call format: accel(m, g, k, y, c, vel)
@@ -51,5 +92,3 @@ function acceleration = accel(m, g, k, y, c, vel)
 
     acceleration = (-m*g - k*y - c * vel) / m;
 end
-
-%% 2. Try different c and k values
