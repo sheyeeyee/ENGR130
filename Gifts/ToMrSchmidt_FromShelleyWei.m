96ce52eba024391e499a41f43f5eb89008042ab7
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-11-29 14:38:48 -0500

    Thinking about gifts for Mr. Schmidt and Dr. Harper

---
+++ Gifts/ToMrSchmidt_FromShelleyWei.m
@@ -0,0 +1,5 @@
+% user inputs message with an f in it and all the f's are changed to ph's
+% bc phish is his favorite band
+
+% include a joke somewhere about me being the only person at his office
+% hours
