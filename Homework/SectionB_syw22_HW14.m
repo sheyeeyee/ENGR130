63c8bf8d62fe810f9ed8c2aa6d28ed0de446e68a
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-12-03 12:43:39 -0500

    Add comments to HW 14

--- Homework/SectionB_syw22_HW14.m
+++ Homework/SectionB_syw22_HW14.m
@@ -14,17 +14,19 @@ out_phrase = myFunction(in_phrase);
 %% Question 2
 clear; clc; close all;
 
-starData = fopen("HW14_Stars.txt", "r");
+starData = fopen("HW14_Stars.txt", "r"); % open file for reading
 
-stars = 21;
+stars = 21; % set a variable for the number of stars
 
-headers = fscanf(starData, "%s", 4);
+headers = fscanf(starData, "%s", 4); % scan in the headers into a separate variable
 
+% initialize the data vectors
 name = "";
 dist = [];
 specType = "";
 appMag = [];
 
+% for-loop for iterating through every star and organizing respective data into respective vectors
 for i = 1:stars
     name(i) = fscanf(starData, "%s", 1);
     dist(i) = fscanf(starData, "%f", 1);
@@ -32,23 +34,25 @@ for i = 1:stars
     appMag(i) = fscanf(starData, "%f", 1);
 end
 
-fclose(starData);
+fclose(starData); % close file
 
-[numK, perK] = specK(specType, stars); % use function to find the number of stars and percentage of stars with spec type K
+[numK, perK] = specK(specType, stars); % use Function 1 to find the number of stars and percentage of stars with spec type K
 fprintf("The number of stars with spectral type K is %i and the percentage of stars with spectral type K is %.3f%%.\n", numK, perK);
 
-[furthest, closest] = farClose(dist, name);
+[furthest, closest] = farClose(dist, name); % use Function 2 to find the furthest and closest stars, excluding the sun
 fprintf("The farthest star from Earth is %s. The closest is %s.\n", furthest, closest);
 
-absMag = absoluteMagnitude(appMag, dist);
+absMag = absoluteMagnitude(appMag, dist); % calculate the absolute magnitude of all of the stars using Function 3 and place into a vector
 
+% ask user for the star they'd like to see more information on
 userStar = input("\nPlease type the corresponding number to the star you would like to see information on.\n   1) Sol   2) Sirius   3) Canopus   4) Arcturus   5) AlphaCentauri   6) Vega   7) Capella\n   8) Rigel   9) Procyon   10) Achernar   11) Betelgeuse   12) Hadar   13) Altair   14) Aldebaran\n   15) Spica   16) Antares   17) Pollux   18) Fomalhaut   19) Deneb   20) Mimosa   21) Regulus\n");
 
-[uName, uDist, uApp, uAbs] = userStarInfo(userStar, stars, name, dist, appMag);
+[uName, uDist, uApp, uAbs] = userStarInfo(userStar, stars, name, dist, appMag, absMag); % use Function 4 to find the respective data for the user's star
 fprintf("Name: %s\nDistance from Earth: %.3f parsecs\nApparent Magnitude: %.3f\nAbsolute Magnitude: %.3f\n", uName, uDist, uApp, uAbs);
 
-starCalc = fopen("StarCalcs.txt", "w");
+starCalc = fopen("StarCalcs.txt", "w"); % open a new file to write data into
 
+% for-loop for inputting all of the original star data into the file and adding in absolute magnitude data
 for i = 1:stars
     fprintf(starCalc, "%s\t", name(i));
     fprintf(starCalc, "%f\t", dist(i));
@@ -57,7 +61,7 @@ for i = 1:stars
     fprintf(starCalc, "%f\n", absMag(i));
 end
 
-fclose(starCalc);
+fclose(starCalc); % close the new file
 
 %% Functions
 % Question 1
@@ -146,24 +150,36 @@ function absoluteMag = absoluteMagnitude(appMag, distances)
     % Output
     %   absoluteMag: vector of absolute magnitudes of the stars
     
-    absoluteMag = appMag - 5 * log10(distances) + 5;
+    absoluteMag = appMag - 5 * log10(distances) + 5; % use given formula to calculate absolute magnitude of all stars
 end
 
 % Function 4
-function [n, d, app, abs] = userStarInfo(userStarNum, numStars, names, distances, appMags)
-    % comment block
+function [n, d, app, abs] = userStarInfo(userStarNum, numStars, names, distances, appMags, absMags)
+    % Finds the respective data of the user's star and outputs the data as variables
+    % Call format: userStarInfo(userStarNum, numStars, names, distances, appMags)
+    % Inputs
+    %   userStarNum: the corresponding number to the user's star
+    %   numStars: number of stars
+    %   names: vector of names of all of the stars
+    %   distances: vector of distances between all of the stars and Earth
+    %   appMags: apparent magnitudes of all of the stars
+    %   absMags: absolute magnitudes of all of the stars
+    % Outputs
+    %   n: user's star's name
+    %   d: user's star's distance from Earth
+    %   app: user's star's apparent magnitude
+    %   abs: user's star's absolute magnitude
     
-    absMags = absoluteMagnitude(appMags, distances);
-    
-    i = 1;
+    i = 1; % initialize indexer variable for while-loop
 
     % MEANINGFUL WHILE-LOOP
-    while i <= numStars    
+    while i <= numStars % iterate until last star is gone through unless the i meets the user's star number
         if (i == userStarNum)
             n = names(i);
             d = distances(i);
             app = appMags(i);
             abs = absMags(i);
+            break;
         end
 
         i = i + 1;
