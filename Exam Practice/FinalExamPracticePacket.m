fb6fb3e7d1aff229d3950edc61410e2d18974a9c
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-12-08 16:00:17 -0500

    Start Final Exam Practice Packet

---
+++ Exam Practice/FinalExamPracticePacket.m
@@ -0,0 +1,19 @@
+%% #
+clear; clc; close all;
+
+a = 1:20
+b = linspace(20, 1, 20)
+c = a ./ b 
+e = 1:length(c)
+
+for d = 1:length(b) 
+    e(d) = e(d) * 5 
+end 
+figure(1) 
+plot(b, a) 
+figure(2) 
+plot(c, a) 
+figure(3) 
+plot(d, a) 
+figure(4) 
+plot(e, a) 
