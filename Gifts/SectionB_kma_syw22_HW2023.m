e98de06b701619824db56fa7e958d87334ee442b
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-12-12 22:34:10 -0500

    Turned remover into function and trying to figure out loop (switched from for- to while-loop)

--- Gifts/SectionB_kma_syw22_HW2023.m
+++ Gifts/SectionB_kma_syw22_HW2023.m
@@ -17,10 +17,36 @@ i = 1;
 
 letter = fscanf(inText, "%c");
 
-% remove "!!!"
+letter1 = removeExtra(letter, '!') % remove "!!!"
+letter2 = removeExtra(letter1, '&') % remove "&&&"
+letter3 = removeExtra(letter2, '+') % remove "+++"
 
 
-% remove "&&&"
+%% Functions
+function letterNew = removeExtra(letterText, removeChar)
+    numChar = length(letterText);
+    % count = 1;
+    i = 1;
 
+    % for i = 1:numChar-count*3
+    %     if (letterText(i) == removeChar && letterText(i+1) == removeChar && letterText(i+2) == removeChar)
+    %         letterText(i+2) = [];
+    %         letterText(i+1) = [];
+    %         letterText(i) = [];
+    % 
+    %         count = count + 1;
+    %     end
+    % end
 
-% remove "+++"
+    while i <= numChar - i*3
+        if (letterText(i) == removeChar && letterText(i+1) == removeChar && letterText(i+2) == removeChar)
+            letterText(i+2) = [];
+            letterText(i+1) = [];
+            letterText(i) = [];
+
+            i = i + 1;
+        end
+    end
+
+    letterNew = letterText;
+end
