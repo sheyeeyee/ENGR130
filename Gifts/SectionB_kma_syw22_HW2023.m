eea6ce1c2c5fc83e513f1e8ad86df2e449876683
sheyeeyee <74998587+sheyeeyee@users.noreply.github.com>
2023-12-17 11:46:57 -0500

    It works! Realized that two counters are needed instead of just one

--- Gifts/SectionB_kma_syw22_HW2023.m
+++ Gifts/SectionB_kma_syw22_HW2023.m
@@ -1,7 +1,7 @@
 % Shelley Wei
 % Christmas Gift for Dr. Harper
 % Started 12/6/23
-% Finished 12//23
+% Finished 12/17/23
 
 %% OBJECTIVE
 % Unveil the Christmas letter!
@@ -15,24 +15,30 @@ letter = ""; % initialize letter variable to compile all words into
 
 letter = fscanf(inText, "%c"); % scan characters of the text file to include spaces
 
-letter1 = removeExtra(letter, '!') % remove "!!!"
+letter1 = removeExtra(letter, '!'); % remove "!!!"
 letter2 = removeExtra(letter1, '&'); % remove "&&&"
 letter3 = removeExtra(letter2, '+'); % remove "+++"
+
+disp(letter3); % display the readable letter
 
 %% Functions
 function letterNew = removeExtra(letterText, removeChar)
     numChar = length(letterText);
-    i = 1;
+    remove = [removeChar, removeChar, removeChar];
 
-    while i <= numChar - i*3
-        if (letterText(i) == removeChar && letterText(i+1) == removeChar && letterText(i+2) == removeChar)
+    i = 1;
+    count = 1;
+
+    while i <= numChar - count*3
+        if (strcmp(letterText(i:i+2), remove))
             letterText(i+2) = [];
             letterText(i+1) = [];
             letterText(i) = [];
 
-            i = i + 1;
+            count = count + 1;
         end
+        i = i + 1;
     end
 
-    letterNew = letterText
+    letterNew = letterText;
 end
